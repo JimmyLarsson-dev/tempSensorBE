52099a0b2d3d1065fd23f0ccff9646992d4bbd61
Jimmy <Jimmy>
2025-11-25 22:45:40 +0100

    1st commit

---
+++ tempSensorBE/src/main/java/org/tempsensor/tempsensor/TempSensorBeApplication.java
@@ -0,0 +1,13 @@
+package org.tempsensor.tempsensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSensorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempSensorBeApplication.class, args);
+    }
+
+}
---
+++ tempSensorBE/src/test/java/org/tempsensor/tempsensor/TempSensorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tempsensor.tempsensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempSensorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
